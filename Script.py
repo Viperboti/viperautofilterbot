5c7c61333779d4822d90c438f13dc604343fe7bd
Aman81999 <89643233+Aman81999@users.noreply.github.com>
2022-02-14 23:00:02 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,7 +13,7 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b> 
-- Source - https://t.me/asb_backup  
+- Source - @Professor9116  
 
 <b>DEVS:</b>
 - <a href=https://t.me/asb_backup>CLICK HERE</a>"""
