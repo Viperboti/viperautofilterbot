d41af3cf0a6d660231848ba62b3f9f353f165fa3
Aman81999 <89643233+Aman81999@users.noreply.github.com>
2022-02-22 23:07:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('📢 Updates', url='https://t.me/asb_backup')
+                InlineKeyboardButton('📢 Updates', url='https://t.me/+MdrMdUMf3CU4N2Rl')
             ],
             [
                 InlineKeyboardButton('💡 More', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -45,7 +45,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search Movies', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/asb_backup')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/+MdrMdUMf3CU4N2Rl')
             ],[
             InlineKeyboardButton('💡 More', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -88,7 +88,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search Movies', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/asb_backup')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/+MdrMdUMf3CU4N2Rl')
             ],[
             InlineKeyboardButton('💡 More', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
